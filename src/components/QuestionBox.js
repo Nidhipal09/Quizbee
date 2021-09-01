d4d2e42396e2358a5fc41eb4c8d5f0b1708c8bb6
Nidhi pal <nidhipalmms@gmail.com>
2021-09-01 18:31:46 +0530

    Design with bootstrap is done

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Card } from 'react-bootstrap'
+import { Card , Button} from 'react-bootstrap'
 
 const QuestionBox = ({ question, options, selected }) => {
     const [answer, setAnswer] = useState(options)
@@ -7,19 +7,19 @@ const QuestionBox = ({ question, options, selected }) => {
     var randomItem = borderColor[Math.floor(Math.random()*borderColor.length)];
 
     return (
-        <Card border={randomItem} className="mb-3 border-top-0 border-end-0 border-bottom-0 border-5 shadow-sm w-60">
+        <Card border={randomItem} className="mb-3 border-top-0 border-end-0 border-bottom-0 border-5 shadow w-60">
             <Card.Body>
                 <Card.Title>
                     <h5>{question}</h5>
                 </Card.Title>
                 <Card.Text>
                     {answer.map((text, index) => (
-                        <button type="button" className="btn btn-primary m-1" key={index} onClick={() => {
+                        <Button variant="secondary m-1" key={index} onClick={() => {
                             setAnswer([text]);
                             selected(text);
                         }}>
                             {text}
-                        </button>
+                        </Button>
                     ))}
                 </Card.Text>
             </Card.Body>
--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import QuestionBox from './components/QuestionBox'
-import Result from './components/Result'
 import quizService from './OfflineAPI/quizService'
-
 import 'bootstrap/dist/css/bootstrap.css';
-import { Container } from 'react-bootstrap'
-
+import { Container, Card, Button, Row, Col, Modal } from 'react-bootstrap'
+import { BsFillQuestionCircleFill } from "react-icons/bs";
+import { AiFillCheckCircle } from "react-icons/ai";
 
 class Quizbee extends Component {
+    constructor(props) {
+        super(props);
 
+    }
     state = {
         questionBank: [],
         score: 0,
-        responses: 0
+        responses: 0,
+        showModal: true
     };
+
+    setShowModal = () => {
+        this.setState({
+            showModal: false
+        })
+
+    }
 
     getQuestions = () => {
         quizService().then((question) => {
@@ -36,39 +46,77 @@ class Quizbee extends Component {
     }
 
     playAgain = () => {
+
         this.getQuestions();
         this.setState({
             score: 0,
             responses: 0
         })
     }
+
     componentDidMount() {
         this.getQuestions();
-
     }
-
-
+   
+    
     render() {
         return (
-            <Container>
-                <h1>Quiz starts here</h1>
-                {this.state.questionBank.length > 0 &&
-                    this.state.responses < 5 &&
-                    this.state.questionBank.map(({
-                        question, answers, correct, questionId }) => (
-                        <QuestionBox
-                            question={question}
-                            options={answers}
-                            key={questionId}
-                            selected={(answer) => this.computeAnswer(answer, correct)}
-                        />
-                    ))
-                }
+            <>
 
-                {this.state.responses == 5 ?
-                (<Result score={this.state.score} playAgain={this.playAgain} />)
-                : null}
-            </Container>
+                <Container className="mt-5 mb-5">
+                    {this.state.showModal &&
+                        <Card>
+                            <Card.Body>
+                                <Card.Header>
+                                    <Card.Title>
+                                        <Row>
+                                            <Col xs={6}><h1> <BsFillQuestionCircleFill /> &nbsp; Quiz starts here</h1></Col>
+                                            <Col xs={4}></Col>
+                                            <Col xs={2} className="mt-2"><Button variant="outline-primary" onClick={this.playAgain}>Get new questions</Button></Col>
+                                        </Row>
+                                    </Card.Title>
+                                </Card.Header>
+
+                                <Card.Text>
+                                    {this.state.questionBank.length > 0 &&
+                                        this.state.responses < 5 &&
+                                        this.state.questionBank.map(({
+                                            question, answers, correct, questionId }) => (
+                                            <QuestionBox
+                                                question={question}
+                                                options={answers}
+                                                key={questionId}
+                                                selected={(answer) => this.computeAnswer(answer, correct)}
+                                            />
+                                        ))
+                                    }
+
+                                    {this.state.responses == 5 && this.state.showModal ?
+                                        (
+                                            <Modal show={true}>
+                                                <Modal.Header>Result</Modal.Header>
+                                                <Modal.Body><h5>you scored {this.state.score} / 5 correct answers</h5></Modal.Body>
+                                                <Modal.Footer>
+                                                    <Button variant="outline-primary" onClick={this.playAgain}>Play again!</Button>
+                                                    <Button variant="outline-warning" onClick={this.setShowModal}>Cancel</Button>
+                                                </Modal.Footer>
+                                            </Modal>)
+                                        : null}
+
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    }
+                    {!this.state.showModal &&
+                        <div className="text-center m-5">
+                            <h2>Thank you for attending!  < AiFillCheckCircle /></h2>
+                        </div>
+
+                        
+
+                    }
+                </Container>
+            </>
         );
     }
 }
